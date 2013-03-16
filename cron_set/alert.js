35b4412ed0750bd255d132379b61f2ed6d6384c0
otiai10 <otiai10@gmail.com>
2013-03-16 15:57:26 +0900

    fix twitter conf

--- cron_set/alert.js
+++ cron_set/alert.js
@@ -1,7 +1,7 @@
 var util = require('../my_modules/util.js').util;
 var conf = require('../my_modules/conf.js').getConf();
 
-var mess = '@otiai10 [[ALERT]]' + conf.now() + ' Something going wrong on SoundHook Server!! Please check me!!!';
+var mess = '@otiai10 [[ALERT on ' + conf.serv_state + ']]' + conf.now() + ' Something going wrong on SoundHook Server!! Please check me!!!';
 util.bot_tweet({message: mess}, function(err,data){
     if(err){ console.log(err); }
 });
