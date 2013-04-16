d41ef538e572ee6f3e89376875ce4f843cff17a2
otiai10 <otiai10@gmail.com>
2013-04-17 00:52:09 +0900

    tutorial effect

--- public/javascripts/tutorial.js
+++ public/javascripts/tutorial.js
@@ -2,7 +2,24 @@
  * tutorial.js
 **/
 
-(function(){
-  var hiddens = document.getElementsByClassName('tutorial-hidden');
-  console.log(hiddens);
-})();
+var tutorial_index = 0;
+$(function(){
+  showNext();
+  $("div.tutorial-hidden").on('click',function(){
+    nextPage();
+  });
+});
+
+function nextPage(){
+  hideAll();
+  showNext();
+}
+
+function showNext(){
+  tutorial_index = tutorial_index + 1;
+  $("div#tutorial0" + tutorial_index).hide().fadeIn(100);
+}
+
+function hideAll(){
+  $("div.tutorial-hidden").fadeOut(100);
+}
